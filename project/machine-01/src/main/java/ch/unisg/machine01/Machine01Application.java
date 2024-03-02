93dcb1d259125851764801d6fce678eded439ef0
nikokelx <nikolaj.keller@protonmail.com>
2024-03-02 17:37:50 +0100

    Init machine 01 microservice

---
+++ project/machine-01/src/main/java/ch/unisg/machine01/Machine01Application.java
@@ -0,0 +1,13 @@
+package ch.unisg.machine01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Machine01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Machine01Application.class, args);
+    }
+
+}
---
+++ project/machine-01/src/test/java/ch/unisg/machine01/Machine01ApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.unisg.machine01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Machine01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
