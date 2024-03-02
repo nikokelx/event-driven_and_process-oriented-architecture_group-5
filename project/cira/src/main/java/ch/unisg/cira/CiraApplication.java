43cba5fec09bd45408d192a1276ec84c5e2b8002
nikokelx <nikolaj.keller@protonmail.com>
2024-03-02 08:16:19 +0100

    Init cira project

---
+++ project/cira/src/main/java/ch/unisg/cira/CiraApplication.java
@@ -0,0 +1,13 @@
+package ch.unisg.cira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiraApplication.class, args);
+    }
+
+}
