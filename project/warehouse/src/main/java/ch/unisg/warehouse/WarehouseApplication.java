b28993538586b3f6da8a9a3fb06309f56a904c1c
MICHAŁ CISŁO <mumu.cisek@gmail.com>
2024-03-04 23:31:44 +0100

    Add warehouse service with basic functionality

---
+++ project/warehouse/src/main/java/ch/unisg/warehouse/WarehouseApplication.java
@@ -0,0 +1,13 @@
+package ch.unisg.warehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseApplication.class, args);
+	}
+
+}
---
+++ project/warehouse/src/main/java/ch/unisg/warehouse/controller/KafkaController.java
@@ -0,0 +1,22 @@
+package ch.unisg.warehouse.controller;
+
+import ch.unisg.warehouse.service.ProducerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/kafka")
+public class KafkaController {
+
+    @Autowired
+    private ProducerService producerService;
+
+    @GetMapping(value = "/publish")
+    public String sendMessageToKafkaTopic(@RequestParam("message") String message) {
+        producerService.sendMessage(message);
+        return "Successfully published message..!";
+    }
+}
