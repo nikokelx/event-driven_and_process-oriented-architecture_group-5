a232b723751c276c33fe52171c12034a15e89264
MICHAŁ CISŁO <mumu.cisek@gmail.com>
2024-03-05 20:35:28 +0100

    Add init project test-07 and configure docker-compose.yml to create 2 brokers

---
+++ assignments/assignment-1/test-07/README.md
@@ -0,0 +1,9 @@
+# Test 7: Acknowledgement configuration
+
+## Introduction
+
+## Setup
+
+## Conduct the experiment
+
+## Conclusion
---
+++ assignments/assignment-1/test-07/producer/src/main/java/ch/unisg/producer/Producer.java
@@ -0,0 +1,44 @@
+package ch.unisg.producer;
+
+import com.google.common.io.Resources;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Producer
+{
+    private static final String[] global_events = {"project_build", "project_start", "project_end"};
+    public static void main( String[] args ) throws IOException
+    {
+        KafkaProducer<String, String> kafkaProducer;
+        try (InputStream props = Resources.getResource("producer.properties").openStream()) {
+            Properties properties = new Properties();
+            properties.load(props);
+            kafkaProducer = new KafkaProducer<>(properties);
+        }
+
+        System.out.println("Amount of partitions " + kafkaProducer.partitionsFor("project_events").size());
+
+
+        try {
+            for (int i = 0; i < 100000; i++) {
+                kafkaProducer.send(new ProducerRecord<String, String>(
+                        "project_events", // topic
+                        "project_id_" + i, //key
+                        "some_value_" + System.nanoTime()) //value
+                );
+
+                if (i % 100 == 0) {
+                    System.out.println("Sent message number " + i);
+                }
+            }
+        } catch (Throwable throwable) {
+            System.out.println(throwable.getStackTrace());
+        } finally {
+            kafkaProducer.close();
+        }
+    }
+}
