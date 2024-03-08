265882bce574cbd2e686c7dfa78dc317d50127a0
nikokelx <nikolaj.keller@protonmail.com>
2024-03-08 23:36:18 +0100

    Init microservice Logistics && Add Logistics to docker infrastructure.

---
+++ project/logistics/src/test/java/ch/unisg/logistics/LogisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.unisg.logistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
