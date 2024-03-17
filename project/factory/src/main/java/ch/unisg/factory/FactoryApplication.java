94628d4e5516b05c2d9ca0061bbda6d05bdf3c5b
MICHAŁ CISŁO <mumu.cisek@gmail.com>
2024-03-17 12:25:15 +0100

    !Do not merge to main!

--- project/factory/src/main/java/ch/unisg/factory/FactoryApplication.java
+++ project/factory/src/main/java/ch/unisg/factory/FactoryApplication.java
@@ -1,9 +1,11 @@
 package ch.unisg.factory;
 
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableProcessApplication
 public class FactoryApplication {
 
     public static void main(String[] args) {
